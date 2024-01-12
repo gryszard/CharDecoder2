ce9fea3c8555e2d20487b6cef906d4d089b54695
Ryszard Gromek <ryszard.gromek@sharpsoft.pl>
2024-01-12 23:13:07 +0100

    Add project files.

---
+++ CharDecoder/MachineLearning/MNIST.cs
@@ -0,0 +1,121 @@
+﻿using System.IO;
+using System.Windows.Media;
+using NumSharp;
+
+namespace CharDecoder.MachineLearning
+{
+    public static class MNIST
+    {
+        public static List<int> Labels { get; set; } = new();
+        public static List<List<double>> Pixels { get; set; } = new();
+        public static List<NDArray> NDLabels { get; set; } = new();
+
+
+        public static void GetData()
+        {
+            using (var reader = new StreamReader(@"./Resources/mnist_train.csv"))
+            {
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split(',');
+
+                    var label = int.Parse(values[0]);
+
+                    Pixels.Add(values.Skip(1).Select(x => int.Parse(x) / 255.0).ToList());
+
+                    Labels.Add(label);
+                    NDLabels.Add(np.eye(10)[label]);
+                }
+            }
+        }
+    }
+
+    public static class Trainer
+    {
+        private static NDArray W_I_H { get; set; }
+        private static NDArray W_H_O { get; set; }
+        private static NDArray B_I_H { get; set; }
+        private static NDArray B_H_O { get; set; }
+
+        public static int Check(double[] pixels)
+        {
+            var image2 = np.reshape(pixels, (28*28, 1));
+
+            // Forward propagation input -> hidden
+            var h_pre = B_I_H + np.matmul(W_I_H, image2);
+            var h = 1 / (1 + np.exp(-1 * h_pre));
+
+            // Forward propagation hidden -> output
+            var o_pre = B_H_O + np.matmul(W_H_O, h);
+            var o = 1 / (1 + np.exp(-1 * o_pre));
+
+            var result = np.argmax(o);
+
+            return result;
+        }
+
+        public static void Train()
+        {
+            W_I_H = np.random.uniform(-0.5, 0.5, (20, 784));
+            W_H_O = np.random.uniform(-0.5, 0.5, (10, 20));
+            B_I_H = np.zeros((20, 1));
+            B_H_O = np.zeros((10, 1));
+
+            double learnRate = 0.01;
+            int epochs = 3;
+            
+            for (int e = 0; e < epochs; e++)
+            {
+                Console.WriteLine($"Epoc: {e + 1}");
+                int nrCorrect = 0;
+
+                for (int img = 0; img < MNIST.Pixels.Count; img++)
+                {
+                    var image = MNIST.Pixels[img].ToArray();
+                    var image2 = np.reshape(image, (784, 1));
+
+                    var labels = MNIST.NDLabels[img];
+                    labels = np.reshape(labels, (10, 1));
+
+                    // Forward propagation input -> hidden
+                    var h_pre = B_I_H + np.matmul(W_I_H, image2);
+                    var h = 1 / (1 + np.exp(-1 * h_pre));
+
+                    // Forward propagation hidden -> output
+                    var o_pre = B_H_O + np.matmul(W_H_O, h);
+                    var o = 1 / (1 + np.exp(-1 * o_pre));
+
+                    // Cost / Error calculation.
+                    var t1 = np.power((o - labels), 2);
+                    var suma = t1.ToArray<double>().Sum();
+                    var error = 1.0 / (o.size) * suma;
+
+                    // Counting correct matches.
+                    if (np.argmax(o) == np.argmax(labels))
+                    {
+                        nrCorrect++;
+                    }
+
+                    // Backpropagation output -> hidden (cost function derivative)
+                    var delta_o = o - labels;
+                    W_H_O += -learnRate * np.matmul(delta_o, np.transpose(h));
+                    B_H_O += -learnRate * delta_o;
+
+                    // Backpropagation hidden -> input (activation function derivative)
+                    var delta_h = np.matmul(np.transpose(W_H_O), delta_o) * (h * (1 - h));
+                    W_I_H += -learnRate * np.matmul(delta_h, np.transpose(image2));
+                    B_I_H += -learnRate * delta_h;
+
+                    if (img % 10000 == 0)
+                    {
+                        Console.WriteLine($"Epoc: {e + 1}: img: {img} / {MNIST.Pixels.Count}");
+                    }
+                }
+
+                var acc = 1.0 * nrCorrect / MNIST.Pixels.Count;
+                Console.WriteLine($"Acc [{e}] = {(acc * 100)} %");
+            }
+        }
+    }
+}
---
+++ CharDecoder/MainWindow.xaml.cs
@@ -0,0 +1,141 @@
+﻿using CharDecoder.MachineLearning;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace CharDecoder
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        static WriteableBitmap writeableBitmap;
+        static int columnCount = 28;
+        static int rowCount = columnCount;
+        static double finalDpi = (0.5) * columnCount;
+        static double dpiDivisor = 96.0 / finalDpi;
+
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            RenderOptions.SetBitmapScalingMode(image, BitmapScalingMode.NearestNeighbor);
+            RenderOptions.SetEdgeMode(image, EdgeMode.Aliased);
+
+            writeableBitmap = new WriteableBitmap(
+                columnCount,
+                rowCount,
+                finalDpi,
+                finalDpi,
+                PixelFormats.Bgr32,
+                null);
+
+            image.Source = writeableBitmap;
+
+            image.Stretch = Stretch.None;
+            image.HorizontalAlignment = HorizontalAlignment.Left;
+            image.VerticalAlignment = VerticalAlignment.Top;
+
+            image.MouseMove += new MouseEventHandler(i_MouseMove);
+            image.MouseLeftButtonDown +=
+                new MouseButtonEventHandler(i_MouseLeftButtonDown);
+            image.MouseRightButtonDown +=
+                new MouseButtonEventHandler(i_MouseRightButtonDown);
+
+            Console.WriteLine("Getting data");
+            MNIST.GetData();
+
+            Console.WriteLine("Data read");
+
+            Console.WriteLine("Training");
+            Trainer.Train();
+        }
+
+        void DrawPixel(MouseEventArgs e, byte[] colorData)
+        {
+            var x = (int)(e.GetPosition(image).X / dpiDivisor);
+            var y = (int)(e.GetPosition(image).Y / dpiDivisor);
+
+            if (x < 0 || columnCount <= x)
+            {
+                return;
+            }
+
+            if (y < 0 || rowCount <= y)
+            {
+                return;
+            }
+
+            Int32Rect rect = new Int32Rect(x, y, 1, 1);
+
+            writeableBitmap.WritePixels(rect, colorData, 4, 0);
+        }
+
+        void i_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DrawPixel(e, [0, 0, 0, 0]);
+        }
+
+        void i_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DrawPixel(e, [255, 255, 255, 0]);
+        }
+
+        void i_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DrawPixel(e, [255, 255, 255, 0]);
+            }
+            else if (e.RightButton == MouseButtonState.Pressed)
+            {
+                DrawPixel(e, [0, 0, 0, 0]);
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int stride = writeableBitmap.PixelWidth * writeableBitmap.Format.BitsPerPixel / 8;
+            int size = stride * writeableBitmap.PixelHeight;
+
+            byte[] buffer = new byte[size];
+            writeableBitmap.CopyPixels(buffer, stride, 0);
+
+            var inputSize = size / 4;
+
+            double[] buffer2 = new double[inputSize];
+
+            for (int i = 0; i < inputSize; i++)
+            {
+                if (buffer[i * 4] == 0)
+                {
+                    buffer2[i] = 1.0;
+                }
+                else
+                {
+                    buffer2[i] = 0.0;
+                }
+            }
+
+            var result = Trainer.Check(buffer2);
+
+            MessageBox.Show($"Result: {result}");
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            for (int i = 0; i < rowCount; i++)
+            {
+                for (int j = 0; j < columnCount; j++)
+                {
+                    Int32Rect rect = new Int32Rect(j, i, 1, 1);
+                    writeableBitmap.WritePixels(rect, new byte[] { 0, 0, 0, 0 }, 4, 0);
+                }
+            }
+        }
+    }
+}
